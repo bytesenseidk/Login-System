855f7b2b9120c0350ca2ab0547a6bf16300c1a97
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-23 23:15:50 +0200

    update

--- Login.py
+++ Login.py
@@ -1,3 +1,5 @@
+from Encryption import Encrypt
+
 class Login(object):
     def __init__(self, username=None, password=None):
         self.username = username
@@ -5,11 +7,13 @@ class Login(object):
     
     def valid_user(self):
         valid = False
-        with open("users.txt", "r") as file:
+        user_file = Encrypt("users.txt").decryption()
+        with open(user_file, "r") as file:
             text = file.readlines()
             for line in text:
                 for user in line.split("\n"):
                     if self.username == user.split(",")[0] and self.password == user.split(",")[1]:
                         valid = True
+        user_file = Encrypt("users.txt").encrypt()
         return valid    
         
--- SignUp.py
+++ SignUp.py
@@ -1,3 +1,5 @@
+from Encryption import Encrypt
+
 class SignUp(object):
     def __init__(self, username=None, password=None):
         self.username = username
